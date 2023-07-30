4ed529be325f9922154b0f3cdf65ac1d3c588409
ladislav-moravec <118938709+ladislav-moravec@users.noreply.github.com>
2023-07-30 15:48:18 +0200

    use firefox

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
-driver = webdriver.Chrome()
-
+driver = webdriver.Firefox()
 
 driver.get("https://w2.sfgame.net/")
-time.sleep(10000)
+# all_window_handles = driver.window_handles
+# driver.switch_to.window(all_window_handles[0])
+
+try:
+    cookies = WebDriverWait(driver, 50).until(
+        EC.presence_of_element_located((By.ID, "onetrust-accept-btn-handler"))
+    )
+    cookies.click()
+    element = driver.find_element_by_css_selector("#webgl-element")
+    element_text = driver.execute_script("return arguments[0].innerText;", element)
+    print(element_text)
+finally:
+    time.sleep(50)
+    driver.quit()
+
+time.sleep(50)
 
 username = driver.find_element_by_name("username")
-username.send_keys("Sir Ritteboli")
+username.send_keys("Luis04s")
 password = driver.find_element_by_name("password")
 password.send_keys("jkoSEKEC322")
 
